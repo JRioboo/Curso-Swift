00f62929e1da2f2d62b325cec7197710d1d15bde
Jose Rioboo <joserioboo@gmail.com>
2019-04-21 20:13:42 +0200

    Initial Commit

---
+++ Curso Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Curso Swift
+//
+//  Created by José  María Rioboo León on 21/04/2019.
+//  Copyright © 2019 José María Rioboo León. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Curso SwiftTests/Curso_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  Curso_SwiftTests.swift
+//  Curso SwiftTests
+//
+//  Created by José  María Rioboo León on 21/04/2019.
+//  Copyright © 2019 José María Rioboo León. All rights reserved.
+//
+
+import XCTest
+@testable import Curso_Swift
+
+class Curso_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
